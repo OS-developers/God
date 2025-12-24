374ab4ef6f7a9a3b8246d295ef382a71570d0687
OS-developers <macdonellgeareyjack@gmail.com>
2025-12-24 04:49:41 +0000

    Create GodBlessYou.cpp

---
+++ C++/GodBlessYou.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+namespace user
+{
+    std::string input;
+}
+
+void func() 
+{
+    std::cout<<"God bless you"<<std::endl;
+    std::cout<<"Type anything to exit"<<std::endl;
+    std::cout<<"answer> ";
+    std::cin>>user::input;
+}
+
+int main()
+{
+    func();
+    return 0;
+}
